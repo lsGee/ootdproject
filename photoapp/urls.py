e8cea7d9be675e1c629a6a1e10d711613ba57224
Jeryong Ryu <wima90@gmail.com>
2020-08-24 14:55:06 +0900

    Add photo

--- photoapp/urls.py
+++ photoapp/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.photo, name="photo"),
+    path('<int:id>/', views.photo, name="photo"),
+    path('<int:id>/<int:like>/', views.photo, name="photo"),
 ]
