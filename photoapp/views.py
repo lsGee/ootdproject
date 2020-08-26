a6b740ff4daf9b65c6bdaa6c67e37616c597d6d8
Jeryong Ryu <wima90@gmail.com>
2020-08-26 23:09:25 +0900

    너무어렵다 바뀌는게 없다.

--- photoapp/views.py
+++ photoapp/views.py
@@ -4,29 +4,38 @@ from django.template import loader
 from mainapp.models import Sido, City, Image
 from django.core.paginator import Paginator
 from django.templatetags.static import static
+from datetime import date
 
 
-def photo(request, id, like=3):
+def photo(request, id=0, like=3):
+    today = date.today()
     try :
-        page = request.GET.get('Image.id',id)
-        imageList = Image.objects.all()
+        imgDetail = Image.objects.get(id=id)
+        imageList = Image.objects.filter(city_id_id= imgDetail.city_id_id,
+                                         image_date__month=today.month,
+                                         image_date__day=today.day).order_by('image_date').reverse()
+        print(imageList)
+        page = request.GET.get('id',imgDetail.id)
+        print(page)
         paginator = Paginator(imageList,1)
         imageListpage = paginator.get_page(page)
-        imgDetail = Image.objects.get(id=id)
+        print(imageListpage)
+        print(imageListpage.number)
         context = { "imageList": imageListpage, "imgDetail": imgDetail }
-        if like == 0:
-            imgLike = Image.objects.get(id=id)
-            imgLike.image_like += 1
-            imgLike.save()
-            context = { "imageList": imageListpage, "imgDetail": imgDetail, "imgLike.image_like": imgLike.image_like }
-        elif like == 1:
-            imgDislike = Image.objects.get(id=id)
-            imgDislike.image_dislike += 1
-            imgDislike.save()
-            context = { "imageList": imageListpage, "imgDetail": imgDetail, "imgDislike.image_dislike": imgDislike.image_dislike }
+        if request.method == "POST" :
+            if like == 0:
+                imgLike = Image.objects.get(id=id)
+                imgLike.image_like += 1
+                imgLike.save()
+                context = { "imageList": imageListpage, "imgDetail": imgDetail, "imgLike.image_like": imgLike.image_like }
+            elif like == 1:
+                imgDislike = Image.objects.get(id=id)
+                imgDislike.image_dislike += 1
+                imgDislike.save()
+                context = { "imageList": imageListpage, "imgDetail": imgDetail, "imgDislike.image_dislike": imgDislike.image_dislike }
 
 
-    except Image.DoesNotExist:
+    except Image.DoesNotExist :
         context = { "msg" : "게시물이 존재하지 않습니다." }
 
     return render(request, "photo.html", context)
