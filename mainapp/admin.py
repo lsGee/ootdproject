44a90e5d6bc4d25d18bfd00eea12fe956e7a2ac5
Sangji Lee <mchy3018@gmail.com>
2020-08-28 22:59:58 +0900

    Update ootdproject/urls.py and mainapp

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Sido, City, Image
 
 # Register your models here.
+admin.site.register(Sido)
+admin.site.register(City)
+admin.site.register(Image)
