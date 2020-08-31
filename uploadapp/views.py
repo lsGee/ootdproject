2a2f3aacc304215104f111b7dfb03b655b27b92c
Your Name <you@example.com>
2020-08-31 17:43:17 +0900

    kkh

--- uploadapp/views.py
+++ uploadapp/views.py
@@ -7,6 +7,22 @@ from django.templatetags.static import static
 
 
 # Create your views here.
-def upload(request):
 
-    return render(request, "upload.html")
+
+def upload(request):
+    context = None
+    if request.method == 'POST':
+        image = Image(image_name=request.POST['image_name'],
+                      image_like=0,
+                      image_dislike=0,
+                      image_cnt=0,
+                      city_id=request.POST['city_id'],
+                      sido_id=request.POST['sido_id'],
+                      image_file=request.FILES['image_file'])
+        image.save()
+        context = {"image": image,
+                   'sido_list': Sido.objects.all(),
+                   'city_list': City.objects.all(),
+                   }
+
+    return render(request, "upload.html", context)
