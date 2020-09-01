765e732839c83fae3e83f67ae5ca963af61de30d
Sangji Lee <mchy3018@gmail.com>
2020-09-01 18:32:37 +0900

    Update mainapp

--- mainapp/views.py
+++ mainapp/views.py
@@ -20,6 +20,7 @@ def main(request) :
         'sido_list': Sido.objects.all(),
         'city_list': City.objects.all(),
         'image_list': todayImg.order_by("city_id", "image_cnt"),
+        'image_list_like': todayImg.order_by("city_id", "image_like")[:5],
         'image_count': todayImg.count(),
         'image_plike': int(today_like/(today_dislike + today_like)*100)
     }
