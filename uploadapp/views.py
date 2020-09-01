673ed5814a970a4f9ff9c5963b2c910ac189c003
Your Name <you@example.com>
2020-09-01 18:11:20 +0900

    kkk

--- uploadapp/views.py
+++ uploadapp/views.py
@@ -9,6 +9,7 @@ from django.templatetags.static import static
 # Create your views here.
 
 
+# Create your views here.
 def upload(request):
     context = None
     if request.method == 'POST':
@@ -16,13 +17,34 @@ def upload(request):
                       image_like=0,
                       image_dislike=0,
                       image_cnt=0,
-                      city_id=request.POST['city_id'],
-                      sido_id=request.POST['sido_id'],
+                      city_id_id=request.POST.get('city_id', ''),
+                      sido_id_id=City.objects.filter(city_id=request.POST.get('city_id', '')).values('sido_id'),
                       image_file=request.FILES['image_file'])
         image.save()
-        context = {"image": image,
-                   'sido_list': Sido.objects.all(),
-                   'city_list': City.objects.all(),
-                   }
+        context = {"image": image}
+
+    else:
+        context = {'sido_list': Sido.objects.all(),
+                   'city_list': City.objects.all()}
+
+    return render(request, "upload.html", context)
+
+
+def uploadsel(request):
+    context = None
+    if request.method == 'POST':
+        image = Image(image_name=request.POST['image_name'],
+                      image_like=0,
+                      image_dislike=0,
+                      image_cnt=0,
+                      city_id_id=request.POST.get('city_id', ''),
+                      sido_id_id=request.POST.get('sido_id', ''),
+                      image_file=request.FILES['image_file'])
+        image.save()
+        context = {"image": image}
+
+    else:
+        context = {'sido_list': Sido.objects.all(),
+                   'city_list': City.objects.all()}
 
     return render(request, "upload.html", context)
