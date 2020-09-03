27739c329489214ebf6b994b7044003fd5e4b03b
Jeryong Ryu <wima90@gmail.com>
2020-09-03 11:40:48 +0900

    Add template

--- photoapp/views.py
+++ photoapp/views.py
@@ -34,7 +34,7 @@ def photo(request, list):
 
         return render(request, "photo.html", context)
 
-    else :
+    elif data == "today" :
         try :
             if sort !="" :
                 imageList = Image.objects.filter(city_id_id= city,
@@ -63,6 +63,26 @@ def photo(request, list):
 
         return render(request, "photo.html", context)
 
+    else :
+        try:
+            if sort != "":
+                blist = Image.objects.filter(image_date__month=today.month,
+                                             image_date__day=today.day).order_by(sort).reverse()
+            else:
+                blist = Image.objects.filter(image_date__month=today.month,
+                                             image_date__day=today.day).order_by('image_date').reverse()
+            bDetail = blist[list]
+            bDetail.image_cnt += 1;
+            bDetail.save()
+            page = request.GET.get('page', list + 1)
+            paginator = Paginator(blist, 1)
+            bListpage = paginator.get_page(page)
+            context = {"blist": bListpage, "bDetail": bDetail, "sort": sort, "data": data}
+        except Image.DoesNotExist:
+            context = {"msg": "게시물이 존재하지 않습니다."}
+
+        return render(request, "photo.html", context)
+
 def likeCount(request) :
     # if request.method == "POST" :
     id = request.GET.get("id")
@@ -82,3 +102,12 @@ def dislikeCount(request) :
 
     jsonContent={ "dislike": imgDetail.image_dislike }
     return JsonResponse(jsonContent, json_dumps_params={'ensure_ascii': False})
+
+def index(request) :
+    return render(request, "index.html")
+
+def generic(request) :
+    return render(request, "generic.html")
+
+def elements(request) :
+    return render(request, "elements.html")
